1712283d302c2495489080b965e54922a08917ab
aristotelos <arisvd@gmail.com>
2024-03-22 11:09:57 +0100

    Simplify published result
    Remove web.config because we don't need IIS support, do not publish
    appsettings.Development.json.

--- src/FlaUI.WebDriver/Program.cs
+++ src/FlaUI.WebDriver/Program.cs
@@ -1,22 +1,31 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
-using System.IO;
-using System.Reflection;
+using FlaUI.WebDriver;
+using Microsoft.OpenApi.Models;
 
-namespace FlaUI.WebDriver
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<ISessionRepository, SessionRepository>();
+
+builder.Services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
+builder.Services.AddControllers(options =>
+    options.Filters.Add(new WebDriverResponseExceptionFilter()));
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "FlaUI.WebDriver", Version = "v1" });
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-        .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "FlaUI.WebDriver v1"));
 }
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
