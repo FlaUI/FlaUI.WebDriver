02ec54f8ad7b11cffe7db2d45950ae4624f01457
Dor-bl <dblayzer@gmail.com>
2024-04-09 23:09:50 +0300

    fix: Update _driver usage

--- src/FlaUI.WebDriver.UITests/ActionsTest.cs
+++ src/FlaUI.WebDriver.UITests/ActionsTest.cs
@@ -15,37 +15,37 @@ namespace FlaUI.WebDriver.UITests
         public void Setup()
         {
             var driverOptions = FlaUIDriverOptions.TestApp();
-            driver = new RemoteWebDriver(WebDriverFixture.WebDriverUrl, driverOptions);
+            _driver = new RemoteWebDriver(WebDriverFixture.WebDriverUrl, driverOptions);
         }
 
         [TearDown]
         public void Teardown()
         {
-            driver?.Dispose();
+            _driver?.Dispose();
         }
 
         [Test]
         public void PerformActions_KeyDownKeyUp_IsSupported()
         {
-            var element = driver.FindElement(ExtendedBy.AccessibilityId("TextBox"));
+            var element = _driver.FindElement(ExtendedBy.AccessibilityId("TextBox"));
             element.Click();
 
-            new Actions(driver).KeyDown(Keys.Control).KeyDown(Keys.Backspace).KeyUp(Keys.Backspace).KeyUp(Keys.Control).Perform();
-            string activeElementText = driver.SwitchTo().ActiveElement().Text;
+            new Actions(_driver).KeyDown(Keys.Control).KeyDown(Keys.Backspace).KeyUp(Keys.Backspace).KeyUp(Keys.Control).Perform();
+            string activeElementText = _driver.SwitchTo().ActiveElement().Text;
             Assert.That(activeElementText, Is.EqualTo("Test "));
         }
 
         [Test]
         public void ReleaseActions_Default_ReleasesKeys()
         {
-            var element = driver.FindElement(ExtendedBy.AccessibilityId("TextBox"));
+            var element = _driver.FindElement(ExtendedBy.AccessibilityId("TextBox"));
             element.Click();
-            new Actions(driver).KeyDown(Keys.Control).Perform();
+            new Actions(_driver).KeyDown(Keys.Control).Perform();
 
-            driver.ResetInputState();
+            _driver.ResetInputState();
 
-            new Actions(driver).KeyDown(Keys.Backspace).KeyUp(Keys.Backspace).Perform();
-            string activeElmentText = driver.SwitchTo().ActiveElement().Text;
+            new Actions(_driver).KeyDown(Keys.Backspace).KeyUp(Keys.Backspace).Perform();
+            string activeElmentText = _driver.SwitchTo().ActiveElement().Text;
             Assert.That(activeElmentText, Is.EqualTo("Test TextBo"));
         }
     }
