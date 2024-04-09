b0e544bb45718977b4d8a8a0fd1808d15907df32
Dor-bl <dblayzer@gmail.com>
2024-04-09 23:32:15 +0300

    Avoid using relative path for WpfApplication

--- src/FlaUI.WebDriver.UITests/TestUtil/TestApplication.cs
+++ src/FlaUI.WebDriver.UITests/TestUtil/TestApplication.cs
@@ -1,9 +1,24 @@
 using System.IO;
+using System.Linq;
 
 namespace FlaUI.WebDriver.UITests.TestUtil
 {
     public static class TestApplication
     {
-        public static string FullPath => Path.GetFullPath("..\\..\\..\\..\\TestApplications\\WpfApplication\\bin\\Release\\WpfApplication.exe");
-    }
+
+        private static readonly string _currentDirectory = Directory.GetCurrentDirectory();
+        private static readonly string _solutionDirectory = FindSolutionDirectory(_currentDirectory);
+
+        private static string FindSolutionDirectory(string currentDirectory)
+        {
+            while (!Directory.GetFiles(currentDirectory, "*.sln").Any())
+            {
+                currentDirectory = Directory.GetParent(currentDirectory).FullName;
+            }
+            return currentDirectory;
+        }
+
+        public static string FullPath => Path.Combine(_solutionDirectory, "TestApplications", "WpfApplication", "bin", "Release", "WpfApplication.exe");
+    } 
+    
 }
