b58018fb4a90943128faf5796b14fd1b1186ffad
aristotelos <arisvd@gmail.com>
2024-05-14 21:17:53 +0200

    Thread safe adding and evicting of elements
    Prevent stale enumerators and concurrency issues while adding or
    victing unavailable elements or windows. Closes #42.

--- src/FlaUI.WebDriver/KnownWindow.cs
+++ src/FlaUI.WebDriver/KnownWindow.cs
@@ -4,21 +4,22 @@ namespace FlaUI.WebDriver
 {
     public class KnownWindow
     {
-        public KnownWindow(Window window, string? windowRuntimeId)
+        public KnownWindow(Window window, string? windowRuntimeId, string windowHandle)
         {
             Window = window;
             WindowRuntimeId = windowRuntimeId;
-            WindowHandle = Guid.NewGuid().ToString();
+            WindowHandle = windowHandle;
         }
-        public string WindowHandle { get; set; }
+
+        public string WindowHandle { get; }
 
         /// <summary>
         /// A temporarily unique ID, so cannot be used for identity over time, but can be used for improving performance of equality tests.
         /// "The identifier is only guaranteed to be unique to the UI of the desktop on which it was generated. Identifiers can be reused over time."
         /// </summary>
         /// <seealso href="https://learn.microsoft.com/en-us/windows/win32/api/uiautomationclient/nf-uiautomationclient-iuiautomationelement-getruntimeid"/>
-        public string? WindowRuntimeId { get; set; }
+        public string? WindowRuntimeId { get; }
 
-        public Window Window { get; set; }
+        public Window Window { get; }
     }
 }
